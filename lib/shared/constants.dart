96f33c1e948eda7f5ad3ecfa76f6ac846227c338
Naidu <naidugudivada768@gmail.com>
2022-08-24 11:14:45 +0530

    commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:1047919157292:web:8cce895a601442379d3189";
+  static String apiKey = "AIzaSyBCPF5Kw67lI49VPQIcuzual7-7hy4hUiQ";
+  static String messagingSenderId = "1047919157292";
+  static String projectId = "chatappfirebase-e2a19";
+  final primaryColor = const Color(0xFFee7b64);
+}
